9c164fe803fc20e49b290e2fea5827ff989c4016
Jack Franklin <jack@jackfranklin.net>
2014-09-29 00:06:07 +0300

    add cli

---
+++ cli.rb
@@ -0,0 +1,84 @@
+require_relative 'world'
+
+# starting_board = gets
+
+# # 1,0:2,0:3,0
+
+# board = []
+# starting_board.split(":").each do |cell|
+#   board.push(cell.split(",").map(&:to_i))
+# end
+
+board = [[2, 1], [3, 2], [1, 3], [2, 3], [3, 3]]
+
+world = World.new(board)
+
+def find_smallest_x(world)
+  smallest_x = 99
+  world.board.each do |cell|
+    if cell[0] < smallest_x
+      smallest_x = cell[0]
+    end
+  end
+  smallest_x
+end
+
+def find_largest_x(world)
+  largest_x = -99
+  world.board.each do |cell|
+    if cell[0] > largest_x
+      largest_x = cell[0]
+    end
+  end
+  largest_x
+end
+
+def find_smallest_y(world)
+  smallest_y = 99
+  world.board.each do |cell|
+    if cell[1] < smallest_y
+      smallest_y = cell[1]
+    end
+  end
+  smallest_y
+end
+
+def find_largest_y(world)
+  largest_y = -99
+  world.board.each do |cell|
+    if cell[1] > largest_y
+      largest_y = cell[1]
+    end
+  end
+  largest_y
+end
+
+def print_world(world)
+  smallest_x = find_smallest_x(world)
+  smallest_y = find_smallest_y(world)
+  largest_x = find_largest_x(world)
+  largest_y = find_largest_y(world)
+
+  smallest_y = 0 if smallest_y > 0
+  smallest_x = 0 if smallest_x > 0
+
+  (smallest_y..largest_y).each do |y|
+    out = ""
+    (smallest_x..largest_x).each do |x|
+      if world.board.include?([x, y])
+         out = out + "A"
+      else
+        out = out + " "
+      end
+    end
+    puts out
+  end
+end
+
+while true
+  print_world(world)
+  world = world.tick
+  sleep 0.5
+  puts ''
+end
+
--- world.rb
+++ world.rb
@@ -4,49 +4,62 @@ class World
   end
 
   def tick
-    new_world = []
-    @cells.each do |cell|
-      neighbours = neighbours_for_cell(cell)
-      if [2, 3].include?(neighbours)
-        new_world.push(cell)
-      end
-    end
-    self.class.new((new_world + check_dead_cells_for_new_life).uniq)
+    self.class.new(next_cells)
   end
 
   def board
     @cells
   end
 
-  def check_dead_cells_for_new_life
-    new_cells = []
-    @cells.each do |live_cell|
-      cell_x, cell_y = live_cell
-      each_surrounding_cells_for_cell_do(live_cell) do |x, y|
-        next if cell_x == x && cell_y == y || @cells.include?([x, y])
-        if neighbours_for_cell([x, y]) == 3
-          new_cells.push([x, y])
-        end
-      end
-    end
-    new_cells.uniq
-  end
-
   def neighbours_for_cell(cell)
     neighbour_count = 0
-    each_surrounding_cells_for_cell_do(cell) do |x, y|
-      if @cells.include?([x, y]) && [x, y] != cell
+    surrounding_cells_for_cell_each(cell) do |surrounding_cell|
+      if cell_is_alive?(surrounding_cell) && surrounding_cell != cell
         neighbour_count = neighbour_count + 1
       end
     end
     neighbour_count
   end
 
-  def each_surrounding_cells_for_cell_do(cell)
+  private
+
+  def cell_is_alive?(cell)
+    @cells.include?(cell)
+  end
+
+  def find_dead_cells_to_live
+    [].tap do |new_cells|
+      @cells.each do |live_cell|
+        surrounding_cells_for_cell_each(live_cell) do |surrounding_cell|
+          if !cell_is_alive?(surrounding_cell) &&
+              cell_has_three_neighbours?(surrounding_cell)
+            new_cells.push(surrounding_cell)
+          end
+        end
+      end
+    end
+  end
+
+  def cell_remains_alive?(cell)
+    [2, 3].include?(neighbours_for_cell(cell))
+  end
+
+  def next_cells
+    new_world = @cells.select(&method(:cell_remains_alive?))
+    (new_world + find_dead_cells_to_live).uniq
+  end
+
+
+  def cell_has_three_neighbours?(cell)
+    neighbours_for_cell(cell) == 3
+  end
+
+
+  def surrounding_cells_for_cell_each(cell)
     x, y = cell
     (x-1..x+1).each do |current_x|
       (y-1..y+1).each do |current_y|
-        yield(current_x, current_y)
+        yield([current_x, current_y])
       end
     end
   end
